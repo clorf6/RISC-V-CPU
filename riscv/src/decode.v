ddc794137d6c727744fd9818ec4d84afeee384c8
clorf6 <2628474577@qq.com>
2023-12-11 11:33:43 +0800

    ✨ feat(riscv): Add instruction decode

--- riscv/src/decode.v
+++ riscv/src/decode.v
@@ -16,6 +16,54 @@ module Decode (
     wire [31:25] funct7 = inst[31:25];
     
     always @(*) begin
+        case (opcode)
+            7'b0110011: begin
+                name = funct7 == 7'b0000000 ? `ADD : `SUB;
+                rd   = inst[11:7];
+                rs1  = inst[19:15];
+                rs2  = inst[24:20];
+            end
+            7'b0000011: begin
+                name = `LW;
+                rd   = inst[11:7];
+                rs1  = inst[19:15];
+                imm  = {{21{inst[31]}}, inst[30:20]};
+            end
+            7'b0100011: begin
+                name = `SW;
+                rs1  = inst[19:15];
+                rs2  = inst[24:20];
+                imm  = {{21{inst[31]}}, inst[30:25], inst[11:7]};
+            end
+            7'b1100011: begin
+                name = `BEQ;
+                rs1  = inst[19:15];
+                rs2  = inst[24:20];
+                imm  = {{20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0}; 
+            end
+            7'b0110111: begin
+                name = `LUI;
+                rd   = inst[11:7];
+                imm  = {inst[31:12], 12'b0};
+            end
+            7'b0010111: begin
+                name = `AUIPC;
+                rd   = inst[11:7];
+                imm  = {inst[31:12], 12'b0};
+            end
+            7'b1101111: begin
+                name = `JAL;
+                rd   = inst[11:7];
+                imm  = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
+            end
+            7'b1100111: begin
+                name = `JALR;
+                rd  = inst[11:7];
+                rs1 = inst[19:15];
+                imm = {{21{inst[31]}}, inst[30:20]};
+            end
+
+        endcase
     end
 
 endmodule
